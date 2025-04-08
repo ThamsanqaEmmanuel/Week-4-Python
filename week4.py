e0e2ac2f37624f6907ad68e9ece0df6582bcf325
ThamsanqaEmmanuel <ethamsanqa@gmail.com>
2025-04-09 00:56:10 +0200

    Update week4.py

--- week4.py
+++ week4.py
@@ -1,31 +1,16 @@
-def read_and_write_file():
-    #CAPTURE FILE NAME
-    filename = input("Enter the name of the file to read: ")
-
-    try:
-        
-        with open( filename, 'r') as file:
-    
-            content = file.read()
-
-        # Modify the content
-        modified_content = content.upper()
-
-        
-        output_filename = input("Enter the name of the file to write the modified content to: ")
-
-       
-        with open(output_filename, 'w') as output_file:
-            
-            output_file.write(modified_content)
-        
-        print(f"File was successfully read and written to {output_filename}.")
-    
-    except FileNotFoundError:
-        print(f"Error: The file '{filename}' does not exist.")
-    except IOError:
-        print(f"Error: There was a problem reading or writing the file.")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-
-read_and_write_file()
+#reading the file and printing it's contents
+try:
+    #Reading existing file
+    filename = input("Enter the name of the file you want to read:")
+    with open(filename, "r") as file:
+        data = file.read()
+        print(data) 
+#Creating and writing a new file
+    filename2 = input("Enter the name of the new file you want to create:")
+    new_content = input("Enter the new content you want to add:")
+    with open(filename2, 'w') as file:
+        new_file = data + '\n' + new_content
+        file.write(new_file)
+    print(f"created successfully {new_file}")
+except FileNotFoundError: 
+    print("File was not found")
